4b617b9f2c595f65dcac3f33426d444b31e4728e
Deepak Patidar <deepakpatidar11102000@gmail.com>
2023-02-15 20:48:05 +0530

    first commit

---
+++ front-end/login/login.js
@@ -0,0 +1,19 @@
+ function  login(event){
+  event.preventDefault();
+//   console.log(event.target.name);
+
+  const loginDetails = {
+    email: event.target.email.value,
+    password: event.target.password.value
+  }
+  console.log(loginDetails)
+  axios.post('http://localhost:3000/user/login',loginDetails)
+  .then(response => {
+    alert(response.data.message)
+    window.location.href = "../ExpanseTracker/index.html"
+  }).catch(err => {
+    console.log(json.stringify(err))
+    document.body.innerHTML += `<div style="color:red;">${err.message} <div>`
+  })
+
+}
